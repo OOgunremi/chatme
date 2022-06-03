336309b6307c4a9e3fcda69bf99064547d6389e5
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-06-02 23:02:41 -0600

    set up dependencies

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
 import { ChatEngine } from "react-chat-engine";
+import './App.css';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID=""
+      userName=""
+      userSecret=""
+    />
+  )
+}
+
+export default App
