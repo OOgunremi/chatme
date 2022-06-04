9f602b75def0732caa24b288d6e493a7c6dba39b
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-06-03 20:21:16 -0600

    integrate chatme API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { ChatEngine } from "react-chat-engine";
 import './App.css';
-
 import React from 'react'
-
 const App = () => {
   return (
     <ChatEngine
-      height="100vh"
-      projectID=""
-      userName=""
-      userSecret=""
+    projectID=""
+    userName=""
+    userSecret=""
+    height="80vh"
     />
   )
 }
